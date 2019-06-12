94049fc248220fb7a4c88c596b602ab299f3f382
Emerson Jacobson <emwjacobson@gmail.com>
2019-06-11 19:32:49 -0700

    Added home and items pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'items', component: ItemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
